fc76e48bc77d824ebf311e0762647b30140df0ec
Jeremy R <lvlgrinddesign@gmail.com>
2019-05-10 12:07:43 -0400

    Create Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Form = () => (
+        <form>
+            <input type='text'></input>
+            <button>Search</button>
+        </form>
+);
+
+export default Form;
