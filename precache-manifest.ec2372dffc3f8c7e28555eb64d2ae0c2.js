20ec3f248a6bdb667499c4a59e421dd4c55a14b3
Jeremy R <lvlgrinddesign@gmail.com>
2019-05-10 15:30:51 -0400

    Updates

---
+++ precache-manifest.ec2372dffc3f8c7e28555eb64d2ae0c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49c0ad317c33fe73308e5f569f623b6",
+    "url": "/recipe-react-app/index.html"
+  },
+  {
+    "revision": "dc61452d76a7c923af30",
+    "url": "/recipe-react-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "772261ec613082df9149",
+    "url": "/recipe-react-app/static/css/main.e280a3f0.chunk.css"
+  },
+  {
+    "revision": "dc61452d76a7c923af30",
+    "url": "/recipe-react-app/static/js/2.d3c94571.chunk.js"
+  },
+  {
+    "revision": "772261ec613082df9149",
+    "url": "/recipe-react-app/static/js/main.b2d9c3d6.chunk.js"
+  },
+  {
+    "revision": "5e93d6df48753d8dcab4",
+    "url": "/recipe-react-app/static/js/runtime~main.bba7b00d.js"
+  }
+]);
